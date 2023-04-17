aee1c600c5a0c6693e9d47c3bb0e3df33f1e2dc1
Tafarel <tafalopess@gmail.com>
2023-04-17 15:16:36 -0300

    realizado ajustes solicitado pelo cliente e adicionado um banner promocional

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,36 +1,67 @@
 var swiper = new Swiper(".slide-container", {
-    slidesPerView: 4,
-    spaceBetween: 20,
-    sliderPerGroup: 4,
-    centerSlide: "true",
-    fade: "true",
-    grabCursor: "true",
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    520: {
+      slidesPerView: 1,
     },
-  
-    breakpoints: {
-      0: {
-          slidesPerView: 1,
-      },
-      520: {
-          slidesPerView: 1,
-      },
-      800: {
-          slidesPerView: 2,
-      },
-      1150: {
-          slidesPerView: 3,
-      },
-      1350: {
-          slidesPerView: 4,
-      } 
+    800: {
+      slidesPerView: 2,
     },
-  });
-  
+    1150: {
+      slidesPerView: 3,
+    },
+    1350: {
+      slidesPerView: 4,
+    }
+  },
+});
+
+
+
+const popupContainer = document.querySelector(".popup-container");
+const closeBtn = document.querySelector(".popup-close img");
+const popupLinks = document.querySelectorAll(".popup-buttons a");
+
+// Definir valor inicial da chave "displayPopup" como "false"
+localStorage.setItem("displayPopup", false);
+
+if (localStorage.getItem("displayPopup")) {
+  setTimeout(() => {
+    popupContainer.classList.add("active");
+  }, 2000);
+}
+
+const stopDisplayingPopup = () => {
+  localStorage.setItem("displayPopup", true);
+  popupContainer.classList.remove("active");
+  window.onbeforeunload = null;
+};
+
+closeBtn.addEventListener("click", stopDisplayingPopup);
+
+popupLinks.forEach(link => {
+  link.addEventListener("click", stopDisplayingPopup);
+});
+
+window.onbeforeunload = function () {
+  localStorage.removeItem("displayPopup");
+  return '';
+};
